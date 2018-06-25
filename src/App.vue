ebe7c77db97050f9189de6e19f9ec206edab3a42
Dieter Luypaert <dieterluypaert@gmail.com>
2018-06-25 20:00:36 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="app">
+    <h1>Hello Parcel 📦 🚀</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+html,
+body {
+  height: 100%;
+}
+body {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+/* eslint no-new:off */
+
+new Vue(App).$mount('#app');
