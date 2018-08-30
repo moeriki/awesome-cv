db903377e035e9d39df8f016e2143b872517768a
Dieter Luypaert <dieterluypaert@gmail.com>
2018-08-30 19:48:20 +0200

    Add in between headers for clarity

--- src/Curriculum.vue
+++ src/Curriculum.vue
@@ -13,7 +13,13 @@
         <education />
       </v-flex>
       <v-flex xs12>
+        <h4 class="display-1">Experience</h4>
+      </v-flex>
+      <v-flex xs12>
         <employers />
+      </v-flex>
+      <v-flex xs12>
+        <h4 class="display-1">Skills</h4>
       </v-flex>
       <v-flex xs12>
         <skills />
@@ -37,5 +43,8 @@ export default {
 <style scoped>
 .main > div {
   margin-top: 16px;
+}
+h4 {
+  margin-top: 32px;
 }
 </style>
