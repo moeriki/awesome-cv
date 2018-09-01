a5959111c9407f1ea4ab677460394ae4d47de548
Dieter Luypaert <dieterluypaert@gmail.com>
2018-09-01 21:21:07 +0200

    Fix education design

--- src/Education.vue
+++ src/Education.vue
@@ -2,19 +2,23 @@
   <v-layout>
     <v-flex xs12>
       <v-card>
-        <v-card-title primary-title>
-          <v-icon
-            color="primary"
-            large>
-            {{ education.icon }}
-          </v-icon>
-          <h5 class="headline">
-            {{ education.title }}
-            <span class="subheading grey--text">
-              {{ education.location }}, {{ education.dateFrom }} &ndash; {{ education.dateUntil }}
-            </span>
-          </h5>
-        </v-card-title>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon
+                color="primary"
+                large>
+                {{ education.icon }}
+              </v-icon>
+            </v-list-tile-action>
+            <h5 class="headline">
+              {{ education.title }}
+              <span class="subheading grey--text">
+                {{ education.location }}, {{ education.dateFrom }} &ndash; {{ education.dateUntil }}
+              </span>
+            </h5>
+          </v-list-tile>
+        </v-list>
       </v-card>
     </v-flex>
   </v-layout>
