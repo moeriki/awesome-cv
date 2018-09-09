201aa367a2c1dfa068d5c021c1be52d6e6194069
Dieter Luypaert <dieterluypaert@gmail.com>
2018-09-09 17:38:57 +0200

    Remove unneeded eslint configuration

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-/* eslint import/prefer-default-export:off */
-
 export const asArray = (object) =>
   Object.keys(object).reduce((acc, propertyName) => {
     acc.push(Object.assign({ _id: propertyName }, object[propertyName]));
