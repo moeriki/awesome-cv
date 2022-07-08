f91a711f86fb59582468fd6bff66ec19f66adf49
Dieter Luypaert <moeriki@hey.com>
2022-07-08 15:34:26 +0200

    💄 dump Windi for CUBE

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-logical-properties'),
+    require('postcss-nesting'),
+  ],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig(() => ({
-  plugins: [WindiCSS()],
   root: 'src/',
 }));
